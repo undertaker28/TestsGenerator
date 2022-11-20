1e75d72e9f68a7c634df11f3465f05630aa7e401
Pavel Starovoytov <pavel.starovoytovvv@gmail.com>
2022-11-20 14:50:29 +0300

    Add TestsGeneratorLibrary

---
+++ TestsGeneratorLibrary/TestFile.cs
@@ -0,0 +1,13 @@
+﻿namespace TestsGeneratorLibrary
+{
+    public class TestFile
+    {
+        public string Name { get; }
+        public string Code { get; }
+        public TestFile(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+    }
+}
